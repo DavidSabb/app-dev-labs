cf900e2de8853f7d5b79e6be4d7dee052854d1df
DavidSabb <158499251+DavidSabb@users.noreply.github.com>
2025-09-11 20:01:49 -0400

    coffeePricesWork

--- week3/coffeePrices/main.dart
+++ week3/coffeePrices/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
 }
 
 class CoffeeApp extends StatefulWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,8 +45,75 @@ class CoffeeApp extends StatefulWidget {
   }
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<CoffeeApp> createState() => _CoffeeAppState();
+}
+
+class _CoffeeAppState extends State<CoffeeApp>{
+  int total = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coffee Prices"),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'asset/americano.jpg',
+                          width: 300,
+                          height: 240,
+                        ),
+                        Text('Americano'),
+                        Text("Price: 10\$"),
+                        ElevatedButton(onPressed:
+                            () {
+                          setState(() {
+                            total += 10;
+                          });
+                        },
+                            child: Text("+"))
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Image.asset(
+                          'asset/latte.jpg',
+                          width: 300,
+                          height: 240,
+                        ),
+                        Text('Latte'),
+                        Text('Price: 8\$'),
+                        ElevatedButton(onPressed:
+                            () {
+                          setState(() {
+                            total += 8;
+                          });
+                        },
+                            child: Text("+"))
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Total: ${total.toString()}\$")
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
